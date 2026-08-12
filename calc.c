374539dcba945b7d6759d050d453023b5d21b391
Prabhneet09 <prabhneet.32119@stu.upes.ac.in>
2026-08-12 22:43:11 +0530

    day-1
    Initial implementation of a simple calculator program that performs addition, subtraction, multiplication, and division.

---
+++ calc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+
+{
+  int a,b;
+  printf("Sum = %d\n",a+b);
+  printf("Difference= %d\n", a-b);
+  printf("Product= %d\n", a*b);
+  printf("quotient = %d\n", a/b);
+  return 0;
+}
