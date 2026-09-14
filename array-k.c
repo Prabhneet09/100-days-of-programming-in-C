ef2c6965129da27f0f8444747a314e4510549790
Prabhneet09 <prabhneet.32119@stu.upes.ac.in>
2026-09-14 23:56:36 +0530

    day-35

---
+++ array-k.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, k;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &k);
+
+    k = k % n;
+
+    for(int j = 0; j < k; j++)
+    {
+        int last = a[n - 1];
+
+        for(int i = n - 1; i > 0; i--)
+        {
+            a[i] = a[i - 1];
+        }
+
+        a[0] = last;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
